53289cc0a5a723fb111cf98dbdd3c7bbef236a97
Darshan Chaudhari <darshanchaudhari4998@gmail.com>
2025-09-24 21:50:36 +0530

    Upgrade to Angular 16

--- Frontend/src/app/Services/User Gurd/user.guard.ts
+++ Frontend/src/app/Services/User Gurd/user.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { LoginService } from '../Login Services/login.service';
 
 @Injectable({
 providedIn: 'root'
 })
-export class UserGuard implements CanActivate {
+export class UserGuard  {
 constructor(private login:LoginService,private router:Router){}
 canActivate(
 route: ActivatedRouteSnapshot,
